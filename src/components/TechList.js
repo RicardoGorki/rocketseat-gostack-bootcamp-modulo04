5b6d3b1b2735897e56b5a4902f25c08f75ae2955
RicardoGorki <ricardo.gorki@gmail.com>
2019-11-19 20:32:47 -0300

    components

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -5,16 +5,36 @@ import TechItem from './TechItem'
 class TechList extends Component {
   state = {
     newTech: '',
-    techs: [
-      'Node.js',
-      "ReactJS",
-      "ReactNative",
-    ]
+    techs: []
   };
+
+  // Executado assim que o componente aparece em tela
+  componentDidMount(){
+    const techs = localStorage.getItem('techs');
+
+    if(techs){
+      this.setState({techs: JSON.parse(techs)});
+    }
+  }
+
+  // Executado sempre que houver alterações nas props ou estado
+  componentDidUpdate(_, prevState){
+    // this.props, this.state
+
+    if(prevState.techs !== this.state.techs){
+      localStorage.setItem('techs', JSON.stringify(this.state.techs))
+    }
+  }
+
+  // Executado quando o componente deixa de existir
+  componentWillUnmount(){
+
+  }
 
   handleInputChange = e => {
     this.setState({ newTech: e.target.value})
   }
+
   handleSubmit = e => {
     e.preventDefault();
 
